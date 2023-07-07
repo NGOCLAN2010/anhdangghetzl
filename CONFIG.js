60df25cc48a42ffe6b1b20a67254fb53878336ad
NGOCLAN2010 <138888285+NGOCLAN2010@users.noreply.github.com>
2023-07-08 01:08:16 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -6,5 +6,5 @@ const CONFIGDATA = {
   buttonNo: "Không đồng ý! Anh đả đảooo",
   titleModar: "Gửi lời cảnh báo.",
   descriptionModar:
-    "Anh đáng ghét, Anh đáng ghét, Anh đáng ghét điều quan trọng nhắc lại 3 lần </br> Sao toàn làm em suy nghĩ hoài vậy, anh có biết thảm hại nhất là gì không? </br> Là khi em cố gắng nói với anh rằng, anh đang làm tổn thương em, em muốn anh thấu hiểu nói với anh rằng Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    "Anh đáng ghét, Anh đáng ghét, Anh đáng ghét điều quan trọng nhắc lại 3 lần </br> Sao làm em suy nghĩ hoài z, anh có biết thảm hại nhất là gì không? </br> Là khi em cố gắng nói với anh rằng, anh đang làm tổn thương em, em muốn được anh thấu hiểu, dỗ dành nhường nhịn anh một chút nhưng mà anh lại khiến em cảm thấy em là một đứa phiền phức, tự suy diễn,..rồi giờ em cảm thấy giữa mình như có một bức tường khoảng cách, càng ngày càng cao, ngày trở nên không tin tưởng nhau nữa thế...phải không anh.</br> Tại sao vậy??...",
 }; 
